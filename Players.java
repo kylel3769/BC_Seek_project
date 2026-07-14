bbbaddf8451c8b214a4377af2b065e7a4cbe1361
Kyle Lam <kylel220811@gmail.com>
2026-07-14 17:12:41 -0400

    update repo

--- Players.java
+++ Players.java
@@ -14,30 +14,55 @@ public class Players {
     // def will just reduce damage by a %
 
     // [base max Hp, base max atk, base def, base max heal]
-    // [    100    ,     50      ,   0%    ,      25      ]
+    // [    100    ,     80      ,   40%    ,      25      ]
     // premade classes
 
     // might be most op
     // great def; good health; mid atk; bad heal
-    Object[] tank = [];
+    Object[] tank = [#,#,#,#];
 
     // cant heal others lol
     // good heal; mid atk & hp; bad def
-    Object[] healer = [];
+    Object[] healer = [#,#,#,#];
 
     // basically rogue
-    // good atk; mid atk & def; bad heal
-    Object[] attacker = [];
+    // good atk; mid hp & def; bad heal
+    Object[] attacker = [110,#,#,#];
 
     // maybe glasscannon?
     // best atk; worse everything else;
-    Object[] d = [];
+    Object[] d = [#,#,#,#];
 
     // all around class?
     // mid everything
-    Object[] jack = [];
+    Object[] jack = [#,#,#,#];
 
     // secret one js for me
-    Object[] z = [];
+    Object[] z = [250,75,20,50];
 }
 
+/*
+import java.util.*;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    static Scanner scan = new Scanner(System.in);
+    static int secretCode = 0;
+
+    System.out.println("Input code.");
+    RecursionTest(scan.nextInt())
+
+
+  }
+
+  public static void RecursionTest(a){
+    if(a == secretCode){
+      System.out.println("Access Granted");
+    } else{
+      System.out.println("DENIED. Try again");
+      RecursionTest(scan.nextInt());
+    }
+  }
+}
+*/
